256b0520e4593a2d7ce22acf02c1f167368489bd
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-10-30 18:55:17 -0400

    Loading WASM async

--- functions/hello-php.js
+++ functions/hello-php.js
@@ -1,29 +1,29 @@
 import { PhpWeb } from '../PhpWeb.mjs';
 // import binary from '../php-web.wasm';
-export function onRequest(context) {
-    return new Promise((accept, reject) => {
-        const info = {};
 
+export async function onRequest(context) {
+    const info = {};
 
-        return fetch('https://php-cloud.pages.dev/php-web.wasm')
-        .then((response) => response.arrayBuffer())
-        .then((bytes) => WebAssembly.instantiate(bytes, importObject))
-        .then(({instance, module}) => {
-            const php = new PhpWeb({
-                instantiateWasm(info, receive) {
-                    receive(instance);
-                    return instance.exports;
-                },
-            });
-            let output = '';
-            let error = '';
-            php.addEventListener('output', (event) => output += event.detail);
-            php.addEventListener('error', (event) => error += event.detail);
-            
-            return php.addEventListener('ready', () => php.run('<?php echo "Hello, PHP!";'))
-            .then(() => accept(output))
-            .catch(() => reject(error));
-
-        });
+    const php = new PhpWeb({
+        async instantiateWasm(info, receive) {
+            const response = await fetch('https://php-cloud.pages.dev/php-web.wasm');
+            const bytes    = await response.arrayBuffer();
+            const {instance, module} = WebAssembly.instantiate(bytes, info);
+            receive(instance);
+            return instance.exports;
+        },
     });
+
+    let output = '';
+    let error  = '';
+
+    php.addEventListener('output', (event) => output += event.detail);
+    php.addEventListener('error',  (event) => error  += event.detail);
+
+    // php.addEventListener('ready', () => );
+    await php.ready;
+
+    php.run('<?php echo "Hello, PHP!";');
+
+    return new Response(output);
 }
