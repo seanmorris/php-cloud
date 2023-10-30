4d54a5f6ed6bd629ba4b8f54cd9389feeef9bb4c
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-10-30 19:21:56 -0400

    Loading WASM async

--- functions/hello-php.js
+++ functions/hello-php.js
@@ -10,8 +10,8 @@ export async function onRequest(context) {
     const instantiateWasm = (imports, receive) => {
         console.log(imports);
         console.log(receive);
-        const {instance, module} = WebAssembly.instantiate(bytes, imports);
-        receive(instance, module);
+        // const {instance, module} = WebAssembly.instantiate(bytes, imports);
+        // receive(instance, module);
         return {};
     };
 
