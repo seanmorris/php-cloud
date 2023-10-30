2d27cfca9147de550f4141aa3d25370b8a9ab96d
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-10-30 18:10:40 -0400

    Adding PHP

--- functions/hello-php.js
+++ functions/hello-php.js
@@ -1,7 +1,14 @@
 import { PhpWeb } from '../PhpWeb.mjs';
+import { PhpWasm } from '../php-web.wasm';
 export function onRequest(context) {
     return new Promise((accept, reject) => {
-        const php = new PhpWeb;
+        const php = new PhpWeb({
+            instantiateWasm(info, receive) {
+                let instance = new WebAssembly.Instance(PhpWasm, info)
+                receive(instance)
+                return instance.exports
+            },
+        });
         let output = '';
         let error = '';
         php.addEventListener('output', (event) => output += event.detail);
