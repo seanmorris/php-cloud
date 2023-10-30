473dbcfe8403da791fb90064d0f8d4e63b35d478
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-10-30 17:51:06 -0400

    Adding PHP

--- functions/hello-php.js
+++ functions/hello-php.js
@@ -1,19 +1,11 @@
 import { PhpWeb } from '../PhpWeb.mjs';
-
 export async function onRequest(context) {
     const php = new PhpWeb;
     return new Promise((accept, reject) => {
         let output = '';
         let error = '';
-
-        php.addEventListener('output', (event) => {
-            output += event.detail;
-        });
-
-        php.addEventListener('error', (event) => {
-            error += event.detail;
-        });
-
+        php.addEventListener('output', (event) => output += event.detail);
+        php.addEventListener('error', (event) => error += event.detail);
         php.addEventListener('ready', () => php.run('<?php echo "Hello, PHP!";'))
         .then(() => accept(output))
         .catch(() => reject(error));
