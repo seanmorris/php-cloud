cbcb4eb7046bed0105491e688dbcb45234d74d54
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-10-30 19:50:39 -0400

    simplify.

--- functions/hello-php.js
+++ functions/hello-php.js
@@ -4,14 +4,14 @@ export async function onRequest(context) {
     let output = 'undef';
     let error  = 'undef';
 
-    // const response = await fetch('https://php-cloud.pages.dev/php-web.wasm');
-    // const buffer   = await response.arrayBuffer();
+    const response = await fetch('https://php-cloud.pages.dev/php-web.wasm');
+    const buffer   = await response.arrayBuffer();
 
     const php = new PhpWeb({locateFile: (file, prefix) => {
         console.log({file, prefix});
         const url = `https://php-cloud.pages.dev/${file}`;
+        fetch(url,{method:'OPTIONS'}).then(console.log);
         console.log({url});
-        return url;
     }});
 
     // php.addEventListener('output', (event) => output += event.detail);
