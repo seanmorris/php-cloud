15e2087fa713b7288513f2e01958eafb5f0ee4f3
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-10-30 19:24:28 -0400

    simplify.

--- functions/hello-php.js
+++ functions/hello-php.js
@@ -5,25 +5,18 @@ export async function onRequest(context) {
     let error  = '';
 
     const response = await fetch('https://php-cloud.pages.dev/php-web.wasm');
-    const bytes    = await response.arrayBuffer();
+    const buffer   = await response.arrayBuffer();
 
-    const instantiateWasm = (imports, receive) => {
-        console.log(imports);
-        console.log(receive);
-        const {instance, module} = WebAssembly.instantiate(bytes, imports);
-        receive(instance, module);
-        return {};
-    };
+    const php = new PhpWeb({buffer});
 
-    // const php = new PhpWeb({instantiateWasm});
+    php.addEventListener('output', (event) => output += event.detail);
+    php.addEventListener('error',  (event) => error  += event.detail);
 
-    // php.addEventListener('output', (event) => output += event.detail);
-    // php.addEventListener('error',  (event) => error  += event.detail);
+    // php.addEventListener('ready', () => );
+    
+    await php.ready;
 
-    // // php.addEventListener('ready', () => );
-    // await php.ready;
-
-    // php.run('<?php echo "Hello, PHP!";');
+    php.run('<?php echo "Hello, PHP!";');
 
     return new Response(output);
 }
