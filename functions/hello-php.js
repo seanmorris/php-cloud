71dbc1b5be877537bf54b0b555980ba2433d16e9
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-10-30 18:44:11 -0400

    Loading WASM async

--- functions/hello-php.js
+++ functions/hello-php.js
@@ -2,23 +2,24 @@ import { PhpWeb } from '../PhpWeb.mjs';
 // import binary from '../php-web.wasm';
 export function onRequest(context) {
     return new Promise((accept, reject) => {
-        const php = new PhpWeb({
-            instantiateWasm(info, receive) {
-                WebAssembly.instantiateStreaming(fetch('https://php-cloud.pages.dev/php-web.wasm'), info)
-                .then(({instance}) => {
-                    receive(instance);
-                });
-                // let instance = new WebAssembly.Instance(binary, info)
-                // receive(instance)
-                // return instance.exports
-            },
+        const info = {};
+        WebAssembly
+        .instantiateStreaming(fetch('https://php-cloud.pages.dev/php-web.wasm'), info)
+        .then(({instance}) => {
+            const php = new PhpWeb({
+                instantiateWasm(info, receive) {                
+                    receive(instance)
+                    return instance.exports
+                },
+            });
+            let output = '';
+            let error = '';
+            php.addEventListener('output', (event) => output += event.detail);
+            php.addEventListener('error', (event) => error += event.detail);
+            php.addEventListener('ready', () => php.run('<?php echo "Hello, PHP!";'))
+            .then(() => accept(output))
+            .catch(() => reject(error));
+            
         });
-        let output = '';
-        let error = '';
-        php.addEventListener('output', (event) => output += event.detail);
-        php.addEventListener('error', (event) => error += event.detail);
-        php.addEventListener('ready', () => php.run('<?php echo "Hello, PHP!";'))
-        .then(() => accept(output))
-        .catch(() => reject(error));
     });
 }
