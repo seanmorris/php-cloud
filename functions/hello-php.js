cd9a7c72a27082b73fa432da73be98deac99ae98
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-10-30 17:47:55 -0400

    Adding PHP

--- functions/hello-php.js
+++ functions/hello-php.js
@@ -1,6 +1,6 @@
 import { PhpWeb } from '../PhpWeb.mjs';
 
-export function onRequest(context) {
+export async function onRequest(context) {
     const php = new PhpWeb;
     return new Promise((accept, reject) => {
 
