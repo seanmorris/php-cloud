a6af2d7e87b7985d79285a8fa52bc04d43b9afae
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-10-30 19:53:54 -0400

    simplify.

--- functions/hello-php.js
+++ functions/hello-php.js
@@ -7,11 +7,12 @@ export async function onRequest(context) {
     const response = await fetch('https://php-cloud.pages.dev/php-web.wasm');
     const buffer   = await response.arrayBuffer();
 
-    const php = new PhpWeb({locateFile: (file, prefix) => {
+    const php = new PhpWeb({locateFile: async (file, prefix) => {
         console.log({file, prefix});
         const url = `https://php-cloud.pages.dev/${file}`;
-        fetch(url,{method:'OPTIONS'}).then(console.log);
         console.log({url});
+        const or = async fetch(url,{method:'OPTIONS'});
+        console.log(or);
         return url;
     }});
 
