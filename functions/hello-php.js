e5c6f58f98ddab8323c41bac57b9d9d12f8dfe43
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-10-30 18:57:40 -0400

    Loading WASM async

--- functions/hello-php.js
+++ functions/hello-php.js
@@ -7,13 +7,13 @@ export async function onRequest(context) {
     const response = await fetch('https://php-cloud.pages.dev/php-web.wasm');
     const bytes    = await response.arrayBuffer();
     
-    // const php = new PhpWeb({
-    //     async instantiateWasm(info, receive) {
-    //         const {instance, module} = WebAssembly.instantiate(bytes, info);
-    //         receive(instance);
-    //         return instance.exports;
-    //     },
-    // });
+    const php = new PhpWeb({
+        async instantiateWasm(info, receive) {
+            const {instance, module} = WebAssembly.instantiate(bytes, info);
+            receive(instance);
+            return instance.exports;
+        },
+    });
 
     // php.addEventListener('output', (event) => output += event.detail);
     // php.addEventListener('error',  (event) => error  += event.detail);
