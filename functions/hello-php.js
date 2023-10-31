e52efbf5190c15f6d30ba3a50ac59321c151dde3
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-10-30 20:18:58 -0400

    simplify.

--- functions/hello-php.js
+++ functions/hello-php.js
@@ -16,6 +16,7 @@ export async function onRequest(context) {
             const url = `https://php-cloud.pages.dev/${file}`;
             fetch(url,{method:'OPTIONS'}).then
             console.log({url});
+            return url;
         }
     });
 
