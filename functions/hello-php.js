e944db963a99853fd6cafad754a9894e497477fb
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-10-30 20:15:27 -0400

    simplify.

--- functions/hello-php.js
+++ functions/hello-php.js
@@ -4,20 +4,32 @@ export async function onRequest(context) {
     let output = 'undef';
     let error  = 'undef';
 
-    const response   = await fetch('https://php-cloud.pages.dev/php-web.wasm');
+    const response = await fetch('https://php-cloud.pages.dev/php-web.wasm');
     const wasmBinary = await response.arrayBuffer();
 
-    const php = new PhpWeb({wasmBinary});
+    console.log(wasmBinary);
 
-    php.addEventListener('output', (event) => output += event.detail);
-    php.addEventListener('error',  (event) => error  += event.detail);
+    const php = new PhpWeb({
+        wasmBinary,
+        locateFile: (file, prefix) => {
+            console.log({file, prefix});
+            const url = `https://php-cloud.pages.dev/${file}`;
+            fetch(url,{method:'OPTIONS'}).then
+            console.log({url});
+        }
+    });
 
-    await php.ready;
+    // php.addEventListener('output', (event) => output += event.detail);
+    // php.addEventListener('error',  (event) => error  += event.detail);
+
+    // // php.addEventListener('ready', () => );
+    
+    // await php.ready;
 
     output = '';
     error = '';
     
-    php.run('<?php echo "Hello, PHP!";');
+    // php.run('<?php echo "Hello, PHP!";');
 
     return new Response(output);
 }
