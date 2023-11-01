0bf96e340ce893bb7c665f39f958d6cf539e1d81
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-10-31 21:30:51 -0400

    Actually run some code.

--- functions/hello-php.js
+++ functions/hello-php.js
@@ -1,35 +1,30 @@
 import { PhpWeb } from '../PhpWeb.mjs';
 import WasmBinary from '../php-web.wasm';
 
-export async function onRequest(context) {
+export function onRequest(context) {
     let output = 'undef';
     let error  = 'undef';
 
-    const php = new PhpWeb({
-        instantiateWasm(info, receive) {
-            let instance = new WebAssembly.Instance(WasmBinary, info)
-            receive(instance)
-            return instance.exports
-        },
-        locateFile: (file, prefix) => {
-            const url = `https://php-cloud.pages.dev/${file}`;
-            return url;
-        }
+    return new Promise(accept => {
+        const php = new PhpWeb({
+            instantiateWasm(info, receive) {
+                let instance = new WebAssembly.Instance(WasmBinary, info)
+                receive(instance)
+                return instance.exports
+            },
+            locateFile: (file, prefix) => {
+                const url = `https://php-cloud.pages.dev/${file}`;
+                return url;
+            },
+            postRun: () => {
+                output = '';
+                error  = '';
+                php.addEventListener('output', (event) => output += event.detail);
+                php.addEventListener('error',  (event) => error  += event.detail);
+                php.run('<?php echo "Hello, PHP!";');
+
+                accept(new Response(output));
+            }    
+        });
     });
-
-    output = '';
-    error  = '';
-
-    console.log('Starting...');
-
-    php.addEventListener('output', (event) => output += event.detail);
-    php.addEventListener('error',  (event) => error  += event.detail);
-
-    const init = await new Promise(accept => php.addEventListener('ready', () => accept()));
-
-    console.log({init});
-
-    php.run('<?php echo "Hello, PHP!";');
-
-    return new Response(output);
 }
