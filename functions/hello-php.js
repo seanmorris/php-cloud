d76d3f7319d7f4dc877174037e04d1906cf75239
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-10-31 20:31:00 -0400

    Actually run some code.

--- functions/hello-php.js
+++ functions/hello-php.js
@@ -4,7 +4,13 @@ export async function onRequest(context) {
     let output = 'undef';
     let error  = 'undef';
 
-    const php = new PhpWeb();
+    const php = new PhpWeb({
+        locateFile: (file, prefix) => {
+            console.log({file, prefix});
+            const url = `https://php-cloud.pages.dev/${file}`;
+            console.log({url});
+        }
+    });
 
     php.addEventListener('output', (event) => output += event.detail);
     php.addEventListener('error',  (event) => error  += event.detail);
