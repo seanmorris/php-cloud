46bc537aaa739cd97c3ab37d497b63964bf389c0
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-10-31 20:29:59 -0400

    Actually run some code.

--- functions/hello-php.js
+++ functions/hello-php.js
@@ -1,30 +1,17 @@
 import { PhpWeb } from '../PhpWeb.mjs';
-// import wasmBinary from '../php-web.wasm';
 
 export async function onRequest(context) {
     let output = 'undef';
     let error  = 'undef';
 
-    const response   = await fetch('https://php-cloud.pages.dev/php-web.wasm');
-    const wasmBinary = await response.arrayBuffer();
+    const php = new PhpWeb();
 
-    console.log(wasmBinary);
-
-    const php = new PhpWeb({
-        wasmBinary,
-        locateFile: (file, prefix) => {
-            console.log({file, prefix});
-            const url = `https://php-cloud.pages.dev/${file}`;
-            console.log({url});
-        }
-    });
-
-    // php.addEventListener('output', (event) => output += event.detail);
-    // php.addEventListener('error',  (event) => error  += event.detail);
+    php.addEventListener('output', (event) => output += event.detail);
+    php.addEventListener('error',  (event) => error  += event.detail);
 
     // // php.addEventListener('ready', () => );
     
-    // await php.ready;
+    await php.ready;
 
     output = '';
     error = '';
