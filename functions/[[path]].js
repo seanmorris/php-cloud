846e506ed93dc8b00fc7034d442fe160179935df
Sean Morris <640101+seanmorris@users.noreply.github.com>
2023-10-31 22:27:57 -0400

    Actually run some code.

---
+++ functions/[[path]].js
@@ -0,0 +1,37 @@
+import { PhpWeb } from '../PhpWeb.mjs';
+import WasmBinary from '../php-web.wasm';
+
+export function onRequest(context) {
+    
+    const php = new PhpWeb({
+        instantiateWasm(info, receive) {
+            let instance = new WebAssembly.Instance(WasmBinary, info)
+            receive(instance)
+            return instance.exports
+        },
+        locateFile: (file, prefix) => {
+            const url = `https://php-cloud.pages.dev/${file}`;
+            return url;
+        }
+    });
+
+    const { readable, writable } = new TransformStream();
+
+	const writer  = writable.getWriter();
+	const encoder = new TextEncoder();
+
+    return php.binary.then(() =>{
+        const write = event => writer.write(encoder.encode(event.detail));
+
+        php.addEventListener('output', write);
+        php.addEventListener('error',  write);
+        
+        context.waitUntil(php.run(`<?php echo ${JSON.stringify(context.params.path)};`));
+
+        return new Response(readable, {
+            status: '200',
+            statusText: 'OK',
+            'content-type': 'text/html'
+        });
+    });
+}
