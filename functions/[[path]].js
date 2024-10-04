770af6840f837dcc95fd014afba58a94f7ddf001
Sean Morris <640101+seanmorris@users.noreply.github.com>
2024-10-04 10:46:42 -0400

    Adding DEV domain to staticOrigins

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -2,6 +2,7 @@ import { PhpWorker } from '../PhpWorker.mjs';
 import WasmBinary from '../php-worker.mjs.wasm';
 
 const staticOrigins = {
+	'https://php-cloud-dev.pages.dev': 'https://seanmorris.github.io/php-static',
 	'https://php-cloud.pages.dev': 'https://seanmorris.github.io/php-static',
 	'http://localhost:8788': 'http://localhost:8080',
 };
