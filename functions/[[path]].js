de10f3df76792967ccb3c1d454e192000f368797
Sean Morris <640101+seanmorris@users.noreply.github.com>
2024-10-04 11:55:20 -0400

    Rebuilding with 3.1.43.

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -4,7 +4,8 @@ import WasmBinary from '../php-worker.mjs.wasm';
 const staticOrigins = {
 	'https://php-cloud-dev.pages.dev': 'https://seanmorris.github.io/php-static',
 	'https://php-cloud.pages.dev': 'https://seanmorris.github.io/php-static-dev',
-	'http://localhost:8788': 'http://localhost:8080',
+	// 'http://localhost:8788': 'http://localhost:8080',
+	'http://localhost:8788': 'https://seanmorris.github.io/php-static-dev',
 };
 
 export async function onRequest(context)
